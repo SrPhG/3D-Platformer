8e1bd7aa4d866028b42feac3c6564ca09708b083
Clarence Veras <cveras2021@elam.co.uk>
2021-11-10 12:32:58 +0000

    Added physics

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,33 +4,43 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float maxSpeed = 0.02f;
+    public float maxSpeed = 10.0f;
     float camRotation = 0.0f;
     float rotation = 5.00f;
     public float rotationSpeed = 2.0f;
     public float camRotationSpeed = 3.00f;
     GameObject cam;
+    Rigidbody myRigidbody;
+
+    bool isOnGround;
+    public GameObject groundChecker;
+    public LayerMask groundLayer;
+    public float jumpForce = 300.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         cam = GameObject.Find("Main Camera");
+        myRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical") * maxSpeed);
-        transform.position = transform.position + (transform.right * Input.GetAxis("Horizontal") * maxSpeed);
+        Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed + (transform.right * Input.GetAxis("Horizontal") * maxSpeed);
+        myRigidbody.velocity = new Vector3(newVelocity.x, myRigidbody.velocity.y, newVelocity.z);
 
         rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
         transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
 
         camRotation = camRotation - Input.GetAxis("Mouse Y") * camRotationSpeed;
         cam.transform.localRotation = Quaternion.Euler(new Vector3(camRotation, 0.0f, 0.0f));
+
+        isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
+        
     }
 }
 
 // 1. All the floats at the top.
 // 2. Change the + > -.
-// 3. Adding another line and change forward into right and vertical into horizontal.
+// 3. Adding another line and change forww w w  ard into right and vertical into horizontal.
